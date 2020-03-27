c7854f3d17a93941fcf83dedf5d217fe94823e30
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-27 13:17:23 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,7 +8,9 @@ def happy_birthday(birthday_kids)
   birthday_kids.each { |name, age|
     if age < 12 
       puts "Happy Birthday #{name}! You are now #{age} years old!"
-    end 
+    elsif age > 12
+      puts "You are too old for this."
+    end
   }
 end
 
